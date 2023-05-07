043b6a2cada80d7aa0f2aeef6fe80ddbebb87aa7
Bartosz Bud <bartoszbud@gmail.com>
2023-05-07 17:34:42 +0200

    POM modified, Java 20 to 17

---
+++ src/main/java/pl/lab/dbservice/EurekaclientApplication.java
@@ -0,0 +1,9 @@
+package pl.lab.dbservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class EurekaclientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaclientApplication.class, args);
+    }
+}
